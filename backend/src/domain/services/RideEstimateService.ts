87414dd5c7a22ae483d6fe5550e5dc3e576fe3c8
Edson <biduzao.bidu21@gmail.com>
2024-11-23 14:15:30 -0300

    feat: ensuring that drivers are filtered from the best value to the highest

--- backend/src/domain/services/RideEstimateService.ts
+++ backend/src/domain/services/RideEstimateService.ts
@@ -4,6 +4,19 @@ import { drivers } from "../../infrastructure/database/mockDrivers";
 export const rideEstimateService = (
   goggleRouteResponse: any
 ): IRouteResponse => {
+  drivers.map((driver) => {
+    return {
+      id: driver.id,
+      name: driver.name,
+      description: driver.description,
+      vehicle: driver.vehicle,
+      review: driver.review,
+      value:
+        driver.ratePerKm! *
+        goggleRouteResponse.routes[0].legs[0].distance.value.toFixed(2),
+    };
+  });
+
   const data: IRouteResponse = {
     origin: {
       latitude: goggleRouteResponse.routes[0].legs[0].start_location.lat,
@@ -15,18 +28,20 @@ export const rideEstimateService = (
     },
     distance: goggleRouteResponse.routes[0].legs[0].distance.value,
     duration: goggleRouteResponse.routes[0].legs[0].duration.value,
-    options: drivers.map((driver) => {
-      return {
-        id: driver.id,
-        name: driver.name,
-        description: driver.description,
-        vehicle: driver.vehicle,
-        review: driver.review,
-        value:
-          driver.ratePerKm! *
-          goggleRouteResponse.routes[0].legs[0].distance.value.toFixed(2),
-      };
-    }),
+    options: drivers
+      .map((driver) => {
+        return {
+          id: driver.id,
+          name: driver.name,
+          description: driver.description,
+          vehicle: driver.vehicle,
+          review: driver.review,
+          value:
+            driver.ratePerKm! *
+            goggleRouteResponse.routes[0].legs[0].distance.value.toFixed(2),
+        };
+      })
+      .sort((a, b) => a.value - b.value),
     routeResponse: goggleRouteResponse,
   };
   return data;
