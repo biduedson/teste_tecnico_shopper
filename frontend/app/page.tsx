ab58aabc419a010173bc33940bffaf53a5b49709
Edson <biduzao.bidu21@gmail.com>
2024-11-24 01:36:05 -0300

    feat: add  formUser to home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import FormUser from "./components/FormUser";
 
 export default function Home() {
   return (
-    <div className=" flex w-screen h-screen items-center justify-center text-white">
-      Home
+    <div className="fixed top-0 flex w-screen h-screen items-center justify-center text-white">
+      <FormUser />
     </div>
   );
 }
