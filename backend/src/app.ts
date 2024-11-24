f4624a7eba88ffa4ecbdade1a31a186553bb3b12
Edson <biduzao.bidu21@gmail.com>
2024-11-24 01:31:33 -0300

    feat: config cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" }); // Caminho para o arquivo .env na raiz do projeto
+import cors from "cors";
 
 import express from "express";
 import { estimateRideRoute } from "./infrastructure/http/routes/estimateRide";
@@ -8,6 +9,15 @@ import { ridecofirmRoutes } from "./infrastructure/http/routes/rideConfirm";
 import { getRideUserList } from "./infrastructure/http/routes/getRideUserList";
 
 const app = express();
+app.use(cors());
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/ride", estimateRideRoute);
