fb45311d2c8c8854fd2f728400e56964712acc06
Edson <biduzao.bidu21@gmail.com>
2024-11-24 01:34:24 -0300

    feat: add config   .env

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { config } from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+config({ path: `${__dirname}/../.env` });
+
+const nextConfig = {
+    images: {
+        remotePatterns: [{ hostname: "landing.shopper.com.br" }, { hostname: "maps.googleapis.com" }],
+    },
+    env: {
+        GOOGLE_MAPS_API_KEY: process.env.GOOGLE_API_KEY,
+    },
+};
 
 export default nextConfig;
